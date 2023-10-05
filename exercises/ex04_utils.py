62510a5ed8a7b1cd0951979b8628d4b859d978ca
rkras <rkras@ad.unc.edu>
2023-10-04 20:56:32 -0400

    Updated with documentation and fixed is_equal

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -2,36 +2,46 @@
 
 __author__ = "730699792"
 
+
 def all(array: list[int], target: int) -> bool:
-    """Determines if all array is all the target element"""
+    """Determines if all array is all the target element."""
+    # counter for while loop
     i: int = 0
-    all_target: bool = True
-    while(i < len(array)):
+    # iterate through array
+    while (i < len(array)):
+    # if a number different than target is found, return false
         if (array[i] != target):
-            all_target = False
+            return False
         i = i + 1
+    # if exited while loop, without returning, return true
     return True
 
 
 def max(array: list[int]) -> int:
     """Returns the largest int in an array."""
-    if(len(array) == 0):
+    # given that the array is not zero
+    if (len(array) == 0):
         raise ValueError("max() arg is an empty list")
+    # set variable largest to first element and i to the second index
     largest: int = array[0]
     i: int = 1
+    # iterate through array
     while (i < len(array)):
+        # if a number is larger than largest, set largest equal to it
         if (largest < array[i]):
             largest = array[i]
         i = i + 1
+    # return number stored in largest
     return largest
 
 
 def is_equal(array1: list[int], array2: list[int]) -> bool:
     """Determines if two arrays are identical."""
-    assert len(array1) == len(array1) and len(array1) != 0 and len(array2) != 0
     i: int = 0
-    while (i < len(array1)):
-        if (array1[i] != array2[i]):
+    j: int = 0
+    while (i < len(array1) and j < len(array2)):
+        if (array1[i] != array2[j]):
             return False
         i = i + 1
+        j = j + 1
     return True
