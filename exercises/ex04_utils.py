c4f6904f8e801c574f234b26a02e7d3c12216959
rkras <rkras@ad.unc.edu>
2023-10-04 20:36:02 -0400

    Stopped infinite loops

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -2,31 +2,43 @@
 
 __author__ = "730699792"
 
-def all (array : int, target : int) -> bool:
+def all (array : list[int], target : int) -> bool:
     """Determines if all array is all the target element"""
     i : int = 0
-    all_target : bool = True
+    all_target: bool = True
     while (i < len(array)):
         if (array[i] != target):
             all_target = False
+        i = i + 1
     return True
 
 
-def max(array : int) -> int:
+def max(array : list[int]) -> int:
     """Returns the largest int in an array."""
     if (len(array) == 0):
         raise ValueError("max() arg is an empty list")
-    largest : int = array[0]
-    i : int = 1
+    largest: int = array[0]
+    i: int = 1
     while (i < len(array)):
         if (largest < array[i]):
             largest = array[i]
+        i = i + 1
     return largest
 
-def is_equal(array1 : int, array2 : int) -> bool:
+def is_equal(array1 : list[int], array2 : list[int]) -> bool:
     """Determines if two arrays are identical."""
-    i : int = 0
+    i: int = 0
     while (i < len(array1)):
         if (array1[i] != array2[i]):
             return False
+        i = i + 1
     return True
+
+def main() -> None:
+    x: list[int] = [1,2,3]
+    y: list[int] = [1,1,1]
+    print(f"all:\nFalse == {all(x,1)}\nTrue == {all(y,1)}")
+    print(f"max:\n3 == {max(x)}\n1 == {max(y)}")
+    print(f"is_equal:\nFalse == {is_equal(x,y)}\nTrue == {is_equal(x,x)}")
+
+main()
