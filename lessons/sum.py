6b629a5fd7b65416a6ef4425752b1df69e0a0223
rkras <rkras@ad.unc.edu>
2023-10-17 10:17:39 -0400

    added types

--- lessons/sum.py
+++ lessons/sum.py
@@ -2,12 +2,12 @@
 
 __author__ = "730699792"
 
-def w_sum(vals: list[float]):
+def w_sum(vals: list[float]) -> float:
     """Finding the sum of a list using a while loop."""
     # index - a counter for element indices
     # output - storing the total sum being returned
     index: int = 0
-    output = 0
+    output: float = 0
     # Iterate through vals
     while (index < len(vals)):
         # add current value to output
@@ -16,10 +16,10 @@ def w_sum(vals: list[float]):
     return output
 
 
-def f_sum(vals: list[float]):
+def f_sum(vals: list[float]) -> float:
     """Finding the sum of a list using a for-element loop."""
     # output - storing the total sum being returned
-    output = 0
+    output: float = 0
     # Iterate through values in vals
     for num in vals:
         # add current value to output
@@ -27,10 +27,10 @@ def f_sum(vals: list[float]):
     return output
 
 
-def f_range_sum(vals: list[float]):
+def f_range_sum(vals: list[float]) -> float:
     """Finding the sum of a list using a for-range loop."""
     # output - storing the total sum being returned
-    output = 0
+    output: float = 0
     # Iterate through vals
     for index in range(0,len(vals)):
         # add value at current index to output
