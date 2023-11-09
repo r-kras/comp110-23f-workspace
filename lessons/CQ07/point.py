f344dd9d48dd983163dd8fdeda0c899beb851e04
rkras <rkras@ad.unc.edu>
2023-11-09 10:17:12 -0500

    Finishing edits on point for cq08

--- lessons/CQ07/point.py
+++ lessons/CQ07/point.py
@@ -13,7 +13,7 @@ class Point:
     x: float
     y: float
     
-    def __init__(self, x_init: float, y_init: float) -> None:
+    def __init__(self, x_init: float = 0.0, y_init: float = 0.0) -> None:
         """Attribute constructor."""
         self.x = x_init
         self.y = y_init
@@ -26,3 +26,15 @@ class Point:
     def scale(self, factor: int) -> Point:
         """Creating a new point beased on a scale."""
         return Point(self.x * factor, self.y * factor)
+    
+    def __str__(self) -> str:
+        """Converting a point to a string."""
+        return f"x: {self.x}; y: {self.y}"
+    
+    def __mul__(self, factor: int | float) -> Point:
+        """Overloading the multiplication operator."""
+        return Point(self.x * factor, self.y * factor)
+
+    def __add__(self, addend: int | float) -> Point:
+        """Overloading the addition operator."""
+        return Point(self.x + addend, self.y + addend)
---
+++ lessons/message.py
@@ -0,0 +1,27 @@
+"""Class to store a message (operator overload; union types; default parameters)."""
+
+class Message:
+    to: str
+    content: str
+    important: bool
+
+    def __init__(self, recipient: str, message_content: str, importance_flag: bool) -> None:
+        """Construct a message."""
+        self.to = recipient
+        self.content = message_content
+        self.important = importance_flag
+
+    def __str__(self) -> str:
+        """Convert message to a string."""
+        return f'Message to: {self.to}\nImportant? {self.important}\n"{self.content}"'
+
+    def __mul__(self, factor: int) -> None:
+        """Multiplication operator overload."""
+        original: str = self.content
+        for loop_number in range(1, factor):
+            self.content += " " + original
+        
+
+msg: Message = Message("Erin", "Great Job!", False)
+# msg * 100
+print(msg)
